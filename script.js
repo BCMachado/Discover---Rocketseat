5157b9c910ca3efa4faa89e6f2bf81be509f974a
BCMachado <brenomaachado@hotmail.com.br>
2023-11-26 20:52:19 -0300

    adjust "Competências"

--- script.js
+++ script.js
@@ -18,10 +18,37 @@ function toggleMode() {
 }
 
 
+// Função "Sobre Mim"
 function toggleAboutMe() {
 var aboutMe = document.getElementById("about-me");
-aboutMe.style.display = aboutMe.style.display === "none" ? "block" : "none";
+aboutMe.style.display = aboutMe.style.display === "none" ? "flex" : "none";
 }
+
+
+
+// Função "Competências" devido as propriedades de display usei essa para que a página carregada mantenha oculto os ícones e só seja de fato exibida somente clicando sobre ela.
+document.addEventListener("DOMContentLoaded", function () {
+  const competenciasDiv = document.getElementById("competencias")
+
+  function toggleCompetencias() {
+    competenciasDiv.style.display =
+      competenciasDiv.style.display === "none" ? "flex" : "none"
+  }
+
+  // Adicione um ouvinte de eventos ao link "Competências"
+  const competenciasLink = document.querySelector(
+    'a[href="#"][onclick="toggleCompetencias()"]'
+  )
+  competenciasLink.addEventListener("click", toggleCompetencias)
+
+  // Inicialmente, oculte as competências ao carregar a página
+  competenciasDiv.style.display = "none"
+})
+
+
+
+
+
 
 
 
